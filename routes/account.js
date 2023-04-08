86615eb3c582ffe39a8545f512d881509ad4dc5c
EmileIB <emileib.lb@gmail.com>
2023-04-08 16:01:54 +0300

    get account route

--- routes/account.js
+++ routes/account.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
-const { update, changePassword } = require("../controllers/account");
+const {
+  update,
+  getAccount,
+  changePassword,
+} = require("../controllers/account");
 const { validate } = require("../middlewares/validators");
 const { verifyToken } = require("../middlewares/verifyToken");
 
 //=============================
 
-router.route("/").put(verifyToken, validate, update);
+router
+  .route("/")
+  .put(verifyToken, validate, update)
+  .get(verifyToken, validate, getAccount);
 router.route("/change-password").put(verifyToken, validate, changePassword);
 
 module.exports = router;
