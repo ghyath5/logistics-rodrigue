1edc53814b10e5508316320570512ffa0c8fba56
maurice <maurice.arida66@gmail.com>
2023-05-03 16:48:51 +0300

    asdasda

--- models/Deliveriesoccur.js
+++ models/Deliveriesoccur.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const DeliveriesOccur = new mongoose.Schema(
   {
-    number: { type: Number }, // 0 = manually, 1 = one per week, 2 = once per fortnight
+    number: { type: Number, default: 2 }, // 0 = manually, 1 = one per week, 2 = once per fortnight
     name: { type: String },
   },
   { timestamps: true }
